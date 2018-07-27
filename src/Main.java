def4e68ca8dd052a3c49ff09b48688eee22d5cda
Wojciech Niechoj <wojciech.niechoj@enginiety.com>
2018-07-27 12:52:53 +0200

    Skonczony program do rownania kwadratowego

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,39 @@
 public class Main {
 
-    public void rownanieKwadratowe(int a, int b, int c){
+    public static void rownanieKwadratowe(int a, int b, int c){
 
         if(a == 0){
             System.out.println("To nie jest równanie kwadratowe");
         }
         else{
-
+            double delta = (b*b) - (4 * a * c);
+            if (delta == 0){
+                int x = (-b)/(2*a);
+                System.out.println("Istnieje jedno rozwiązanie podwójne: x = " + x);
+            }
+            else if (delta < 0){
+                System.out.println("Równanie nie ma rozwiązania w zbiorze liczb rzeczywistych");
+            }
+            else {
+                double x1 = ((-b) - Math.sqrt(delta)) / (2*a);
+                double x2 = ((-b) + Math.sqrt(delta)) / (2*a);
+                System.out.println("Istnieją dwa rozwiązania: x1 = " + x1 + " oraz x2 = " + x2);
+            }
         }
     }
 
 
     public static void main(String[] args) {
-        String a, b, c ;
-        System.in.read(a);
+
+        System.out.println("Podaj wspolczynnik a równania: ");
+        String a = System.console().readLine();
+        System.out.println("Podaj wspolczynnik b równania: ");
+        String b = System.console().readLine();
+        System.out.println("Podaj wspolczynnik c równania: ");
+        String c = System.console().readLine();
 
 
-        rownanieKwadratowe();
+        rownanieKwadratowe(Integer.parseInt(a), Integer.parseInt(b), Integer.parseInt(c));
 
     }
 }
