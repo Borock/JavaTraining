87921599cf1d29093aae19073803e45b49624161
Wojciech Niechoj <wojciech.niechoj@enginiety.com>
2018-07-27 12:26:49 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public void rownanieKwadratowe(int a, int b, int c){
+
+        if(a == 0){
+            System.out.println("To nie jest równanie kwadratowe");
+        }
+        else{
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        String a, b, c ;
+        System.in.read(a);
+
+
+        rownanieKwadratowe();
+
+    }
+}
